dfd29b971d8733607712a208afd105e6c1b74c1d
“bme_songyuxin” <bme_songyuxin>
2021-01-13 17:12:07 +0800

    cp

---
+++ src/main/java/com/syx/springboot/threespringboot/CP/consumer/Consumer.java
@@ -0,0 +1,26 @@
+package com.syx.springboot.threespringboot.CP.consumer;
+
+
+
+/**
+ * 发布订阅者模式——订阅者
+ *
+ */
+public interface Consumer {
+
+
+    /***
+     * 开始消费，启动线程池中的消费线程
+     */
+    void start();
+
+    /***
+     * 停止消费，会等待线程池中的任务完成
+     */
+    void gracefulStop();
+
+    /***
+     * 强制停止
+     */
+    void forceStop();
+}
