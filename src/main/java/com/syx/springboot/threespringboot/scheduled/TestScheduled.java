7f6660a8b71de91f6820718f3178d15a853fb543
“bme_songyuxin” <bme_songyuxin>
2021-01-14 09:38:52 +0800

    tsb_syxfristcommit

---
+++ src/main/java/com/syx/springboot/threespringboot/scheduled/TestScheduled.java
@@ -0,0 +1,14 @@
+package com.syx.springboot.threespringboot.scheduled;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+public class TestScheduled {
+
+    //@Scheduled(cron = "1/3 * * * * ?"))
+    @Scheduled(cron = "3 * * * * ?" )
+    public void testschedule(){
+        System.out.println("测试定时任务");
+    }
+}
