46c395955cd6f757ecdc8e0575b1552405eefd2a
“bme_songyuxin” <bme_songyuxin>
2021-01-14 15:13:38 +0800

    20210114

---
+++ src/test/java/com/syx/springboot/threespringboot/ThreespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.threespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreespringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
