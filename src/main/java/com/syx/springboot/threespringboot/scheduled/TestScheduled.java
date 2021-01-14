ddcdb9735084d6650bd554fc43c9c7cc64729bd1
“bme_songyuxin” <bme_songyuxin>
2021-01-14 14:23:30 +0800

    dev_syx_frist提交交

--- src/main/java/com/syx/springboot/threespringboot/scheduled/TestScheduled.java
+++ src/main/java/com/syx/springboot/threespringboot/scheduled/TestScheduled.java
@@ -17,5 +17,23 @@ public class TestScheduled {
         System.out.println("测试定时任务");
         System.out.println("测试定时任务");
 
+
+        /**
+         *  查看分支
+         *  git branch
+         *
+         *  新建分支
+         *  git branch syx_dev_frist
+         *
+         *  切换分支
+         *  git checkout syx_dev_frist
+         *
+         *  推送分支
+         *  git push origin syx_dev_frist
+         *
+         *
+         */
+
+
     }
 }
